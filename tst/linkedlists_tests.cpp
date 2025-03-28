92837994f5bf1152c0a13efdcf5b793015ee6390
lc10934 <lsc210003@utdallas.edu>
2025-03-27 22:02:02 -0500

    linkedlists

---
+++ tst/linkedlists_tests.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include "../src/linkedlists.cpp" 
+
+TEST(DoublyLinkedListTest, PushFrontTest) {
+    DoublyLinkedList<int> list;
+    list.push_front(10);
+    list.push_front(20);
+    list.push_front(30);
+    
+    std::vector<int> expected = {30, 20, 10};
+    EXPECT_EQ(list.to_vector(), expected);
+}
+
+// Test: push_back should add elements to the back.
+TEST(DoublyLinkedListTest, PushBackTest) {
+    DoublyLinkedList<int> list;
+    list.push_back(10);
+    list.push_back(20);
+    list.push_back(30);
+    
+    std::vector<int> expected = {10, 20, 30};
+    EXPECT_EQ(list.to_vector(), expected);
+}
+
+// Test: pop_front should remove elements from the front.
+TEST(DoublyLinkedListTest, PopFrontTest) {
+    DoublyLinkedList<int> list;
+    list.push_back(10);
+    list.push_back(20);
+    list.push_back(30);
+    
+    list.pop_front(); // removes 10
+    std::vector<int> expected = {20, 30};
+    EXPECT_EQ(list.to_vector(), expected);
+    
+    list.pop_front(); // removes 20
+    expected = {30};
+    EXPECT_EQ(list.to_vector(), expected);
+    
+    list.pop_front(); // removes 30, list becomes empty
+    expected = {};
+    EXPECT_EQ(list.to_vector(), expected);
+}
+
+// Test: pop_back should remove elements from the back.
+TEST(DoublyLinkedListTest, PopBackTest) {
+    DoublyLinkedList<int> list;
+    list.push_front(10);
+    list.push_front(20);
+    list.push_front(30);
+    
+    list.pop_back(); // removes 10
+    std::vector<int> expected = {30, 20};
+    EXPECT_EQ(list.to_vector(), expected);
+    
+    list.pop_back(); // removes 20
+    expected = {30};
+    EXPECT_EQ(list.to_vector(), expected);
+    
+    list.pop_back(); // removes 30, list becomes empty
+    expected = {};
+    EXPECT_EQ(list.to_vector(), expected);
+}
