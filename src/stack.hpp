cd4eb36c2b24b2af4fe6a8e34307cc9a4239b6bb
lc10934 <lsc210003@utdallas.edu>
2025-04-03 19:37:32 -0500

    stack

--- src/stack.hpp
+++ src/stack.hpp
@@ -1,3 +1,35 @@
 #pragma once
 #include <optional>
-#include <memory>
+#include "vector.hpp"  
+
+template <typename T>
+class Stack {
+public:
+    Stack() = default;
+
+    void push(const T& item) {
+        container_.push_back(item);
+    }
+
+    std::optional<T> pop() {
+        return container_.pop();
+    }
+
+    std::optional<T> top() const {
+        if (container_.size() == 0) {
+            return std::nullopt;
+        }
+        return container_[container_.size() - 1];
+    }
+
+    size_t size() const {
+        return container_.size();
+    }
+
+    bool empty() const {
+        return (container_.size() == 0);
+    }
+
+private:
+    Vector<T> container_;
+};
